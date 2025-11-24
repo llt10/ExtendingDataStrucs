a2c2441676ad25f8c75c9cf3c0e4e862d868f326
Leanne  Thoppil <leannelthoppil@gmail.com>
2025-11-23 22:08:53 -0600

    ...

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+template <typename T> 
+class Que {
+    private:
+    T* data;
+    int capacity;
+    int front;
+    int count;
+
+void growth() {
+    int newCapa = capacity * 2;
+
+}
+
+};
